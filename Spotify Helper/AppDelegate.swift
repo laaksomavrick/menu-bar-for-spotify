b0b6d355952119d9cfbbb225ddc871fa03a39a9a
mlaakso@dev <laakso.mavrick@gmail.com>
2017-10-19 19:28:42 -0400

    Initial Commit

---
+++ Spotify Helper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Spotify Helper
+//
+//  Created by Mavrick Laakso on 2017-10-19.
+//  Copyright © 2017 Lucid Development. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
